92a333a903ca8ed1a6ce1b771d19710ebc2745a6
susmi1231 <pasupuletisusmitha1994@gmail.com>
2019-01-18 19:48:09 +0530

    Maven commit

---
+++ src/main/java/com/hari/demo/demo/App.java
@@ -0,0 +1,13 @@
+package com.hari.demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
